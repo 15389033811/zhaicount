7bde019ff4e0a818b0fd38d2f7eb9b18d6fc67f5
15389033811 <447190095@qq.com>
2022-08-30 00:15:44 +0800

    x

--- src/test/java/com/example/zhaicount/ZhaicountApplicationTests.java
+++ src/test/java/com/example/zhaicount/ZhaicountApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.web.server.WebFilter;
 import com.example.zhaicount.dto.ZhaiResp;
 import com.example.zhaicount.entity.TbZhaiInfo;
 import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import okhttp3.MediaType;
@@ -47,9 +49,10 @@ class ZhaicountApplicationTests {
     @Test
     void contextLoads() throws IOException {
 
-
         Integer pageNumber = 1;
-        String zhaiListUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=100&pageNumber="+pageNumber+"&reportName=RPT_BOND_CB_LIST&columns=ALL&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0";
+        String zhaiListUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=100&pageNumber="
+                + pageNumber
+                + "&reportName=RPT_BOND_CB_LIST&columns=ALL&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0";
         HttpRequest respreq = HttpRequest.get(zhaiListUrl);
         System.out.println(respreq.code());
         // System.out.println(resp);
@@ -57,28 +60,30 @@ class ZhaicountApplicationTests {
         ZhaiResp zhaiResp = JSON.parseObject(resp, ZhaiResp.class);
 
         for (pageNumber = 1; pageNumber <= zhaiResp.getResult().getPages(); pageNumber++) {
-            zhaiListUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=100&pageNumber="+pageNumber+"&reportName=RPT_BOND_CB_LIST&columns=ALL&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0";
+            zhaiListUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=100&pageNumber="
+                    + pageNumber
+                    + "&reportName=RPT_BOND_CB_LIST&columns=ALL&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0";
             String pagePesp = HttpRequest.get(zhaiListUrl).body();
             zhaiResp = JSON.parseObject(pagePesp, ZhaiResp.class);
             System.out.println(zhaiResp.getMessage());
             String queryCode = "";
             for (ZhaiResp.Result.Datum zhaiRespItem : zhaiResp.getResult().getData()) {
                 try {
-                queryCode = zhaiRespItem.getConvertStockCode();
-                TbZhaiInfo tbZhaiInfo = new TbZhaiInfo();
-                tbZhaiInfo.setName(zhaiRespItem.getSecurityNameAbbr());
-                tbZhaiInfo.setGuName(zhaiRespItem.getSecurityShortName());
-                tbZhaiInfo.setType(getType(queryCode));
-                tbZhaiInfo.setCode(zhaiRespItem.getSecurityCode());
-                tbZhaiInfo.setPremiumRatio(zhaiRespItem.getTransferPremiumRatio());
-                tbZhaiInfo.setListingData(zhaiRespItem.getListingDate());
-                if (zhaiRespItem.getSecucode().indexOf(".SH") != -1) {
-                    tbZhaiInfo.setMarket("沪");
-                } else {
-                    tbZhaiInfo.setMarket("深");
-                }
-                tbZhaiInfoMapper.insert(tbZhaiInfo);
-                } catch (Exception e){
+                    queryCode = zhaiRespItem.getConvertStockCode();
+                    TbZhaiInfo tbZhaiInfo = new TbZhaiInfo();
+                    tbZhaiInfo.setName(zhaiRespItem.getSecurityNameAbbr());
+                    tbZhaiInfo.setGuName(zhaiRespItem.getSecurityShortName());
+                    tbZhaiInfo.setType(getType(queryCode));
+                    tbZhaiInfo.setCode(zhaiRespItem.getSecurityCode());
+                    tbZhaiInfo.setPremiumRatio(zhaiRespItem.getTransferPremiumRatio());
+                    tbZhaiInfo.setListingData(zhaiRespItem.getListingDate());
+                    if (zhaiRespItem.getSecucode().indexOf(".SH") != -1) {
+                        tbZhaiInfo.setMarket("沪");
+                    } else {
+                        tbZhaiInfo.setMarket("深");
+                    }
+                    tbZhaiInfoMapper.insert(tbZhaiInfo);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -91,23 +96,24 @@ class ZhaicountApplicationTests {
     @Test
     Double getYuE(String zhaiCode) throws UnsupportedEncodingException {
         String yeUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?columns=ALL&sortColumns=date&sortTypes=2&source=WEB&reportName=RPTA_WEB_KZZ_LS&pageNumber=1&pageSize=1&filter=(";
-        yeUrl += URLEncoder.encode("zcode=\""+zhaiCode+"\"", "utf-8")+")";
+        yeUrl += URLEncoder.encode("zcode=\"" + zhaiCode + "\"", "utf-8") + ")";
         String resp = HttpRequest.get(yeUrl).body();
         YeResp yeResp = JSON.parseObject(resp, YeResp.class);
-        if (yeResp.getResult() == null){
+        if (yeResp.getResult() == null) {
             return -1D;
         }
         Integer pageEnd = yeResp.getResult().getPages();
-        yeUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?columns=ALL&sortColumns=date&sortTypes=2&source=WEB&reportName=RPTA_WEB_KZZ_LS&pageNumber="+pageEnd+"&pageSize=1&filter=(";
-        yeUrl += URLEncoder.encode("zcode=\""+zhaiCode+"\"", "utf-8")+")";
+        yeUrl = "https://datacenter-web.eastmoney.com/api/data/v1/get?columns=ALL&sortColumns=date&sortTypes=2&source=WEB&reportName=RPTA_WEB_KZZ_LS&pageNumber="
+                + pageEnd + "&pageSize=1&filter=(";
+        yeUrl += URLEncoder.encode("zcode=\"" + zhaiCode + "\"", "utf-8") + ")";
         resp = HttpRequest.get(yeUrl).body();
         yeResp = JSON.parseObject(resp, YeResp.class);
         Double reultNum = yeResp.getResult().getData().get(0).getSYFE();
 
-        if (reultNum == null){
+        if (reultNum == null) {
             return -1D;
         }
-        return reultNum/100000000;
+        return reultNum / 100000000;
     }
 
     String getType(String guCode) throws IOException {
@@ -120,7 +126,8 @@ class ZhaicountApplicationTests {
                     .addFormDataPart("query", "所属概念")
                     .addFormDataPart("question", guCode)
                     .addFormDataPart("perpage", "1")
-                    .addFormDataPart("condition", "[{\"indexName\":\"所属概念\",\"indexProperties\":[],\"source\":\"new_parser\",\"type\":\"index\",\"indexPropertiesMap\":{},\"reportType\":\"null\",\"chunkedResult\":\"所属概念\",\"valueType\":\"_所属概念\",\"domain\":\"abs_股票领域\",\"uiText\":\"所属概念\",\"sonSize\":0,\"queryText\":\"所属概念\",\"relatedSize\":0,\"tag\":\"所属概念\"}]")
+                    .addFormDataPart("condition",
+                            "[{\"indexName\":\"所属概念\",\"indexProperties\":[],\"source\":\"new_parser\",\"type\":\"index\",\"indexPropertiesMap\":{},\"reportType\":\"null\",\"chunkedResult\":\"所属概念\",\"valueType\":\"_所属概念\",\"domain\":\"abs_股票领域\",\"uiText\":\"所属概念\",\"sonSize\":0,\"queryText\":\"所属概念\",\"relatedSize\":0,\"tag\":\"所属概念\"}]")
                     .addFormDataPart("query_type", "stock")
                     .addFormDataPart("comp_id", "6257151")
                     .addFormDataPart("source", "Ths_iwencai_Xuangu")
@@ -134,46 +141,102 @@ class ZhaicountApplicationTests {
                     .method("POST", body)
                     .addHeader("Host", "www.iwencai.com")
                     .addHeader("Origin", "http://www.iwencai.com")
-                    .addHeader("Referer", "http://www.iwencai.com/unifiedwap/result?w=%E6%89%80%E5%B1%9E%E6%A6%82%E5%BF%B5&querytype=stock")
+                    .addHeader("Referer",
+                            "http://www.iwencai.com/unifiedwap/result?w=%E6%89%80%E5%B1%9E%E6%A6%82%E5%BF%B5&querytype=stock")
                     .addHeader("hexin", "AzRnd5p46oYBAX5KMqIbkoGgBfmjDVj3mjHsO86VwL9COdon9h0oh")
                     .addHeader("Content-Type", "application/json;charset=utf-8")
                     .build();
             Response response = client.newCall(request).execute();
             jsonObject = new JsonParser().parse(response.body().string()).getAsJsonObject();
-            return jsonObject.get("data").getAsJsonObject().get("data").getAsJsonObject().get("datas").getAsJsonArray().get(0).getAsJsonObject().get("所属概念").toString().replaceAll("\"", "");
+            return jsonObject.get("data").getAsJsonObject().get("data").getAsJsonObject().get("datas").getAsJsonArray()
+                    .get(0).getAsJsonObject().get("所属概念").toString().replaceAll("\"", "");
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println(jsonObject.toString());
             return "";
         }
     }
 
+    @Test
+    void getexeclInfo() throws Exception {
+        Integer page = 1;
+        JsonObject jsonObject = null;
+        HashMap<String, TbZhaiInfo> mapInfo = new HashMap<>();
+        for (page = 1; page <= 6; page++) {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType,
+                    "{\r\n    \"question\": \"可转债\",\r\n    \"perpage\": 100,\r\n    \"page\": " + page
+                            + ",\r\n    \"secondary_intent\": \"conbond\",\r\n    \"log_info\": \"{\\\"input_type\\\":\\\"typewrite\\\"}\",\r\n    \"source\": \"Ths_iwencai_Xuangu\",\r\n    \"version\": \"2.0\",\r\n    \"query_area\": \"\",\r\n    \"block_list\": \"\",\r\n    \"add_info\": \"{\\\"urp\\\":{\\\"scene\\\":1,\\\"company\\\":1,\\\"business\\\":1},\\\"contentType\\\":\\\"json\\\",\\\"searchInfo\\\":true}\",\r\n    \"rsh\": \"Ths_iwencai_Xuangu_gg9mwx2kqn7vb4uhzq0g2is0ymq3ilwg\"\r\n}");
+            Request request = new Request.Builder()
+                    .url("http://www.iwencai.com/customized/chart/get-robot-data")
+                    .method("POST", body)
+                    .addHeader("Origin", "http://www.iwencai.com")
+                    .addHeader("Referer",
+                            "http://www.iwencai.com/unifiedwap/result?w=%E5%8F%AF%E8%BD%AC%E5%80%BA&querytype=conbond&addSign=1661787010195")
+                    .addHeader("hexin-v", "A3wvnxLgMwOFmQff3W9AgZMbTRErdSO2oho0Y1bzi15KPhIPfoXwL_IpBOWl")
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+            Response response = client.newCall(request).execute();
+            String respStr = response.body().string();
+            jsonObject = new JsonParser().parse(respStr).getAsJsonObject();
+            // String xz = jsonObject.get("data").getAsJsonObject().get("answer")
+            // .getAsJsonArray().get(0).getAsJsonObject().get("txt").getAsJsonArray().get(0)
+            // .getAsJsonObject().get("content").getAsJsonObject().get("components")
+            // .getAsJsonArray().get(0).getAsJsonObject().get("data").getAsJsonObject().get("datas")
+            // .getAsJsonArray().get(0).getAsJsonObject().get("code").getAsString();
+            // System.out.println(xz);
+            JsonArray zhaiJsonList = jsonObject.get("data").getAsJsonObject().get("answer")
+                    .getAsJsonArray().get(0).getAsJsonObject().get("txt").getAsJsonArray().get(0)
+                    .getAsJsonObject().get("content").getAsJsonObject().get("components")
+                    .getAsJsonArray().get(0).getAsJsonObject().get("data").getAsJsonObject().get("datas")
+                    .getAsJsonArray();
+            for (JsonElement jsonElement : zhaiJsonList) {
+                TbZhaiInfo tbZhaiInfo = new TbZhaiInfo();
+                System.out.println("-------" + page);
+                System.out.println(jsonElement.getAsJsonObject().get("code").getAsString());
+                // System.out.println(jsonElement.getAsJsonObject());
+                // jsonElement.getAsJsonObject().get("code").getAsString();
+                tbZhaiInfo.setBalance(
+                        jsonElement.getAsJsonObject().get("可转债@最新变动后余额[20220830]").getAsDouble() / Math.pow(1,
+                                8));
+                System.out.println(tbZhaiInfo.getBalance());
+                tbZhaiInfo.setUpRate(jsonElement.getAsJsonObject().get("可转债@涨跌幅[20220830]").getAsDouble());
+                System.out.println(tbZhaiInfo.getUpRate());
+
+                mapInfo.put(jsonElement.getAsJsonObject().get("code").getAsString(),
+                        tbZhaiInfo);
+            }
+
+        }
+    }
 
     @Test
     void parseExcel() throws Exception {
-        //        1、获取文件的路径
-//        1.1、从桌面获取文件
+        // 1、获取文件的路径
+        // 1.1、从桌面获取文件
         FileSystemView fsv = FileSystemView.getFileSystemView();
-        String filePath =  "/Users/nut/code/java/zhaicount/2022-07-28 (1).xls";
-//        1.2、从绝对路径获取文件
-//        String filePath = "D:\\testexcel.xls";
+        String filePath = "/Users/nut/code/java/zhaicount/2022-07-28 (1).xls";
+        // 1.2、从绝对路径获取文件
+        // String filePath = "D:\\testexcel.xls";
 
-//        2、通过流获取本地文件
+        // 2、通过流获取本地文件
         FileInputStream fileInputStream = new FileInputStream(filePath);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
         POIFSFileSystem fileSystem = new POIFSFileSystem(bufferedInputStream);
 
-//        3、创建工作簿对象，并获取工作表1
+        // 3、创建工作簿对象，并获取工作表1
         HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
         HSSFSheet sheet = workbook.getSheet("1");
 
-//        4、从工作表中获取行数，并遍历
+        // 4、从工作表中获取行数，并遍历
         int lastRowIndex = sheet.getLastRowNum();
         System.out.println("总行数为：" + lastRowIndex);
         ArrayList<HashMap> list = new ArrayList<>();
         for (int i = 1; i <= lastRowIndex; i++) {
-//            4.1 获取每行的数据
+            // 4.1 获取每行的数据
             HSSFRow row = sheet.getRow(i);
             if (row == null) {
                 break;
@@ -184,13 +247,14 @@ class ZhaicountApplicationTests {
             if (zhaiCode.indexOf(".") == -1) {
                 continue;
             }
-            String zhaiStr  = zhaiCode.substring(0, zhaiCode.indexOf("."));
+            String zhaiStr = zhaiCode.substring(0, zhaiCode.indexOf("."));
 
-            TbZhaiInfo tbZhaiInfo = tbZhaiInfoMapper.selectOne(new LambdaQueryWrapper<TbZhaiInfo>().eq(TbZhaiInfo::getCode,zhaiStr).last("limit 1"));
-            if (tbZhaiInfo == null){
+            TbZhaiInfo tbZhaiInfo = tbZhaiInfoMapper
+                    .selectOne(new LambdaQueryWrapper<TbZhaiInfo>().eq(TbZhaiInfo::getCode, zhaiStr).last("limit 1"));
+            if (tbZhaiInfo == null) {
                 continue;
             }
-            //更新增长率
+            // 更新增长率
             row.getCell(3).setCellType(Cell.CELL_TYPE_STRING);
             String ratio = row.getCell(3).getStringCellValue();
             if (ratio.equals("--")) {
@@ -199,15 +263,15 @@ class ZhaicountApplicationTests {
                 tbZhaiInfo.setUpRate(Double.parseDouble(row.getCell(3).getStringCellValue()));
             }
 
-
-            //更新余额
+            // 更新余额
             row.getCell(16).setCellType(Cell.CELL_TYPE_STRING);
             String balance = row.getCell(16).getStringCellValue();
 
-            if (balance.indexOf("亿") != -1){
+            if (balance.indexOf("亿") != -1) {
                 tbZhaiInfo.setBalance(Double.parseDouble(balance.substring(0, balance.indexOf("亿"))));
-            } else if (balance.indexOf("万") != -1){
-                tbZhaiInfo.setBalance(Double.parseDouble(balance.substring(0, balance.indexOf("万")).replaceAll(",",""))*0.0001);
+            } else if (balance.indexOf("万") != -1) {
+                tbZhaiInfo.setBalance(
+                        Double.parseDouble(balance.substring(0, balance.indexOf("万")).replaceAll(",", "")) * 0.0001);
             } else {
                 tbZhaiInfo.setBalance(0D);
             }
@@ -215,17 +279,10 @@ class ZhaicountApplicationTests {
             tbZhaiInfoMapper.updateById(tbZhaiInfo);
         }
 
-//        6、关闭资源、输出封装数据
+        // 6、关闭资源、输出封装数据
         bufferedInputStream.close();
         System.out.println(list.toString());
 
     }
-
-
-//    @Test
-//    void () throws Exception {
-
-
-
 
 }
